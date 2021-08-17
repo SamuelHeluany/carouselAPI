6e0725df1d1e67f36a869cc88a3d83c582019c27
Samuel Heluany <samuelheluany@hotmail.com>
2021-08-17 18:30:44 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,66 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  
+  const [data, setData] = useState([])
+  const carousel = useRef(null)
 
   useEffect(() => {
-   axios.get('http://localhost:3000/static/shoes.json').then((response) => response.json()).then(console.log)
+    const shoeItems = async () => {
+      const result = await axios.get('http://localhost:3000/static/shoes.json')
+      setData(result.data);
+    }
+    shoeItems();
+
   }, [])
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth
+  }
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth
+  }
+
+  if (!data || !data.length)
+    return null
 
   return (
     <div className="container">
       <div className="logo">
         <h2>SHOES</h2>
       </div>
-      <div className="carousel">
-        <div className="item">
-          <div className="image">
-            <img src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg" alt="Shoe" />
-          </div>
-          <div className="info">
-            <span className="name">Shoe 1</span>
-            <span className="oldPrice">U$ 299.00</span>
-            <span className="price">U$ 199.00</span>
-          </div>
-        </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">{oldPrice}</span>
+                <span className="price">{price}</span>
+              </div>
+            </div>
+          )
+        })} 
       </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Right" />
+        </button>
+      </div>
+     
     </div>
+    
   );
 }
 
